64afa3ac07687eaa642c47717e27537318a153f8
kmm2459344 <kmm2459344@stu.o-hara.ac.jp>
2024-06-18 13:44:48 +0900

    演習1-1:4値の最大値

---
+++ MaxOfFour/MaxOfFour/MaxOfFour/MaxOfFour.cpp
@@ -0,0 +1,30 @@
+//==============================================
+//  4値の最大値
+//==============================================
+#include <stdio.h>
+#include <stdlib.h>  // srand(),rand()
+#include <time.h>    // time()
+// 関数プロトタイプ
+int MaxOfFour(int a, int b, int c, int d);
+
+int main()
+{
+	srand(time(nullptr));
+	int a = rand() % 100;
+	int b = rand() % 100;
+	int c = rand() % 100;
+	int d = rand() % 100;
+	int ans = MaxOfFour(a, b, c, d);
+	printf("MaxOfFour(%d,%d,%d,%d) => %d\n", a, b, c, d, ans);
+	return 0;
+}
+int MaxOfFour(int a, int b, int c, int d)
+{
+	// ここをコーディングしてください。
+	int max = a;
+	max < b ? max = b : max;
+	max < c ? max = c : max;
+	max < d ? max = d : max;
+
+	return max;
+}
